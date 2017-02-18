8d5b3aeeb38d1b28d1c0f613f077c002f5938bff
Peter Meehan <meehanp2@tcd.ie>
2017-02-18 15:38:07 +0000

    linked slider to api

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
+
+@app.route('/control', methods=['POST'])
+def control():
+    val_string = request.get_data()[1:-1]
+    val = int(val_string)
+    return str(val)
+
+# @app.route('/logo', methods=['POST'])
+# def logo():
+#     # do logo things
+#     return "logo pls"
 
 if __name__ == "__main__":
     app.run()
