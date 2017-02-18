622a02693de7c12fbfa255d951fe09ee67c70ca9
Peter Meehan <meehanp2@tcd.ie>
2017-02-18 15:46:38 +0000

    calling bash command with posted value

--- control-web/script.js
+++ control-web/script.js
@@ -3,15 +3,15 @@ var val = document.getElementById('val');
 document.getElementById('slider').addEventListener('change', function() {
   sliderVal = document.getElementById("slider").value;
 
-  console.log(sliderVal)
+  // console.log(sliderVal)
   val.innerHTML = sliderVal;
 
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://localhost:8000');
+  xhr.open('POST', 'http://localhost:5000/control');
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      alert(xhr.responseText);
+      console.log(xhr.responseText + 'received from api');
     }
   }
   xhr.send(JSON.stringify(sliderVal));
--- flask-api/api.py
+++ flask-api/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
@@ -10,9 +11,16 @@ def index():
 
 @app.route('/control', methods=['POST'])
 def control():
+    # the value from the slider
     val_string = request.get_data()[1:-1]
-    val = int(val_string)
-    return str(val)
+    # the command we want to run to move the motor
+    bashCommand = "echo \"you want to send " + val_string + " to the motor\""
+    # run the command
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    # get the output, if it returns anything
+    output, error = process.communicate()
+    # pass that output back to the browser
+    return output
 
 # @app.route('/logo', methods=['POST'])
 # def logo():
