233f3111e9155f63307227a32efaf95849ebf334
Peter Meehan <meehanp2@tcd.ie>
2017-02-18 18:40:35 +0000

    added emotion detection endpoint

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import subprocess
+import httplib, urllib, base64
 
 app = Flask(__name__)
 CORS(app)
@@ -22,10 +23,33 @@ def control():
     # pass that output back to the browser
     return output
 
-# @app.route('/logo', methods=['POST'])
-# def logo():
-#     # do logo things
-#     return "logo pls"
+@app.route('/detect', methods=['GET'])
+def detect():
+    # do bash thing to get frame from video
+    ### bash thing
+
+    with open('smiles.jpg', 'r') as frame:
+        # load into variable
+        body = frame.read()
+        # set api request headers
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': 'API_KEY',
+            }
+        # set api request params, empty
+        params = urllib.urlencode({})
+        # send the request
+        try:
+            conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+            conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+            return data
+
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+            return "hello"
 
 if __name__ == "__main__":
     app.run()
